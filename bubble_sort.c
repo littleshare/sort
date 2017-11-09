d858f7e9c942cd7f3ee4cedd8a1f45977e1339e1
littleshare <862420675@qq.com>
2017-11-09 11:13:22 +0800

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 
-void PrintNum_Int(int *num);												//整型数据显示函数
+void PrintNum_Int(int *num);											//整型数据显示函数
 void BubbleSort_Int(int *num);											//整型数据冒泡排序
+int CountNum(int *num);
 
 void main()
 {
-	int num[] = {6,5,4,3,2,1};
+	int num[6] = {6,5,4,3,2,1};
 	
-	printf("have no sort\n");													//排序前
+	printf("number size is %d\n",CountNum(num));
+	printf("have no sort\n");										//排序前
 	PrintNum_Int(num);
 	
 	BubbleSort_Int(&num[0]);
-	printf("\nhad sorted.\n");												//排序后
+	printf("\nhad sorted.\n");										//排序后
 	PrintNum_Int(num);
+	
+}
+
+int CountNum(int *num)
+{
+	int cnt=0;
+	
+	while(num[cnt]!=NULL)
+	{
+		cnt++;	
+		
+	}
+	
+	return cnt;
+	
 	
 }
 	    
@@ -20,7 +37,7 @@ void PrintNum_Int(int *num)
 	{
 		int i;
 
-		for(i=0;i<(sizof(num);i++)
+		for(i=0;i<CountNum(num);i++)
 		{
 			printf("%d__",num[i]);	
 		} 
@@ -30,7 +47,7 @@ void PrintNum_Int(int *num)
 void BubbleSort_Int(int *num)
 	{
 		int i,j;
-		int numsize=sizeof(num),tmp;
+		int numsize=CountNum(num),tmp;
 
 		for(i=0;i<numsize-1;i++)
 		{
